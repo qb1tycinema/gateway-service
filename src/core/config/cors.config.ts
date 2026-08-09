24d702c3f302f4897f540f3412722189ac847e2f
qb1ty <arsenbro0707@mail.ru>
2026-08-10 04:21:21 +0500

    fix: format fixed

--- src/core/config/cors.config.ts
+++ src/core/config/cors.config.ts
@@ -1,9 +1,9 @@
-import type { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
-import { ConfigService } from "@nestjs/config";
+import type { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface"
+import { ConfigService } from "@nestjs/config"
 
 export function getCorsConfig(config: ConfigService): CorsOptions {
-    return {
-        origin: config.getOrThrow<string>("HTTP_CORS").split(","),
+	return {
+		origin: config.getOrThrow<string>("HTTP_CORS").split(","),
 		credentials: true
-    }
+	}
 }
--- src/core/config/swagger.config.ts
+++ src/core/config/swagger.config.ts
@@ -1,8 +1,8 @@
-import { INestApplication } from "@nestjs/common";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { INestApplication } from "@nestjs/common"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 export function swaggerConfig(app: INestApplication): void {
-    const swaggerConfig = new DocumentBuilder()
+	const swaggerConfig = new DocumentBuilder()
 		.setTitle("Qb1tyCinema API")
 		.setDescription("API Gateway for Qb1tyCinema microservices")
 		.setVersion("1.0.0")
