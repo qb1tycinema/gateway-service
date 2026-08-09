f5c40117071415e89cf1348be2d579258205d88f
qb1ty <arsenbro0707@mail.ru>
2026-08-10 02:38:18 +0500

    refactor: abstracted the main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { Logger } from "@nestjs/common"
+import { Logger, ValidationPipe } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 import { AppModule } from "./core/app.module"
+import { getCorsConfig, swaggerConfig } from "./core/config"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
@@ -11,23 +11,14 @@ async function bootstrap() {
 	const config = app.get(ConfigService)
 	const logger = new Logger()
 
-	app.enableCors({
-		origin: config.getOrThrow<string>("HTTP_CORS").split(","),
-		credentials: true
-	})
+	app.useGlobalPipes(new ValidationPipe({
+		transform: true,
+		whitelist: true
+	}))
 
-	const swaggerConfig = new DocumentBuilder()
-		.setTitle("Qb1tyCinema API")
-		.setDescription("API Gateway for Qb1tyCinema microservices")
-		.setVersion("1.0.0")
-		.addBearerAuth()
-		.build()
+	app.enableCors(getCorsConfig(config))
 
-	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
-
-	SwaggerModule.setup("/documentation", app, swaggerDocument, {
-		yamlDocumentUrl: "/openapi.yaml"
-	})
+	swaggerConfig(app)
 
 	const port = config.getOrThrow<number>("HTTP_PORT")
 	const host = config.getOrThrow<string>("HTTP_HOST")
