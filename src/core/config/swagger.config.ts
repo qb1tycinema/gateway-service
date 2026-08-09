24b5291276d6cd1a298e4578fceb3951ad1b87e5
qb1ty <arsenbro0707@mail.ru>
2026-08-10 02:37:05 +0500

    chore: creat config directory to store the project configuration

---
+++ src/core/config/swagger.config.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function swaggerConfig(app: INestApplication): void {
+    const swaggerConfig = new DocumentBuilder()
+		.setTitle("Qb1tyCinema API")
+		.setDescription("API Gateway for Qb1tyCinema microservices")
+		.setVersion("1.0.0")
+		.addBearerAuth()
+		.build()
+
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+
+	SwaggerModule.setup("/documentation", app, swaggerDocument, {
+		yamlDocumentUrl: "/openapi.yaml"
+	})
+}
