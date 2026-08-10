afc2e7f209e83af761e0767a94b0f372f85f24eb
qb1ty <arsenbro0707@mail.ru>
2026-08-11 00:46:55 +0500

    feat: registration microservice auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,29 @@
 import { Module } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import { ClientsModule, Transport } from "@nestjs/microservices"
 
 import { AuthController } from "./auth.controller"
+import { AuthClientGrpc } from "./auth.grpc"
 
 @Module({
-	controllers: [AuthController]
+	imports: [
+		ClientsModule.registerAsync([
+			{
+				name: "AUTH_PACKAGE",
+				useFactory: (config: ConfigService) => ({
+					transport: Transport.GRPC,
+					options: {
+						package: "auth.v1",
+						protoPath:
+							"node_modules/@qb1tycinema/contracts/proto/auth.proto",
+						url: config.getOrThrow<string>("AUTH_GRPC_URL")
+					}
+				}),
+				inject: [ConfigService]
+			}
+		])
+	],
+	controllers: [AuthController],
+	providers: [AuthClientGrpc]
 })
 export class AuthModule {}
