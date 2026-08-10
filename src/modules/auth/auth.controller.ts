c65fbd7ebc24d437eab9b83a56baed9dbc933fa8
qb1ty <arsenbro0707@mail.ru>
2026-08-11 00:45:51 +0500

    feat: direct call to the auth microservice

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common"
 import { ApiOperation } from "@nestjs/swagger"
 
+import { AuthClientGrpc } from "./auth.grpc"
 import { SendOtpReguest } from "./dto"
 
 @Controller("auth")
 export class AuthController {
+	public constructor(private readonly client: AuthClientGrpc) {}
+
 	@ApiOperation({
 		summary: "Send otp code",
 		description:
@@ -13,8 +16,6 @@ export class AuthController {
 	@Post("otp/send")
 	@HttpCode(HttpStatus.OK)
 	public async sendOtp(@Body() dto: SendOtpReguest) {
-		console.log("DATA: ", dto)
-
-		return { ok: true }
+		return this.client.sendOtp(dto)
 	}
 }
