35858855512c45a474c008a74fa3c99d68a628c0
qb1ty <arsenbro0707@mail.ru>
2026-08-10 06:13:26 +0500

    fix: format fixed file

--- src/modules/auth/dto/requests/send-otp.request.ts
+++ src/modules/auth/dto/requests/send-otp.request.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsEnum, IsString, Validate } from "class-validator"
 
 import { IdentifierValidator } from "@/shared/validators"
 
 export class SendOtpReguest {
+	@ApiProperty({ example: "+77712169309" })
 	@IsString()
 	@Validate(IdentifierValidator)
 	public identifier!: string
 
+	@ApiProperty({ example: "phone", enum: ["phone", "email"] })
 	@IsEnum(["phone", "email"])
 	public type!: "phone" | "email"
 }
