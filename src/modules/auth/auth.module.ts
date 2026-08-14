7bc96a71cfa1867cba448837069c58cfb46cf620
qb1ty <arsenbro0707@mail.ru>
2026-08-15 00:33:41 +0500

    chore: remove hardcode path

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { ClientsModule, Transport } from "@nestjs/microservices"
+import { PROTO_PATHS } from "@qb1tycinema/contracts"
 
 import { AuthController } from "./auth.controller"
 import { AuthClientGrpc } from "./auth.grpc"
@@ -14,8 +15,7 @@ import { AuthClientGrpc } from "./auth.grpc"
 					transport: Transport.GRPC,
 					options: {
 						package: "auth.v1",
-						protoPath:
-							"node_modules/@qb1tycinema/contracts/proto/auth.proto",
+						protoPath: PROTO_PATHS.AUTH,
 						url: config.getOrThrow<string>("AUTH_GRPC_URL")
 					}
 				}),
