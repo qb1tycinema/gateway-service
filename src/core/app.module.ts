acfd250155031a99e616ec46142ae116b6e341a9
qb1ty <arsenbro0707@mail.ru>
2026-08-20 21:45:07 +0500

    chore: setup root app module dependencies

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { PassportModule } from "@qb1tycinema/passport"
 
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { getPassportConfig } from "./config"
 import { AuthModule } from "@/modules/auth/auth.module"
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true
+		}),
+		PassportModule.registerAsync({
+			useFactory: getPassportConfig,
+			inject: [ConfigService]
 		}),
 		AuthModule
 	],
