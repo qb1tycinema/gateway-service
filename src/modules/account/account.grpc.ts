f926b39d1bcc77f6793d3229056a2116f62e5364
qb1ty <arsenbro0707@mail.ru>
2026-08-24 06:39:38 +0500

    feat(account): implement grpc client wrappers for contact change workflows

--- src/modules/account/account.grpc.ts
+++ src/modules/account/account.grpc.ts
@@ -2,7 +2,11 @@ import { Inject, Injectable, OnModuleInit } from "@nestjs/common"
 import type { ClientGrpc } from "@nestjs/microservices"
 import type {
 	AccountServiceClient,
-	GetAccountRequest
+	ConfirmEmailChangeRequest,
+	ConfirmPhoneChangeRequest,
+	GetAccountRequest,
+	InitEmailChangeRequest,
+	InitPhoneChangeRequest
 } from "@qb1tycinema/contracts/gen/account"
 
 @Injectable()
@@ -21,5 +25,21 @@ export class AccountClientGrpc implements OnModuleInit {
 
 	public getAccount(request: GetAccountRequest) {
 		return this.accountService.getAccount(request)
+	}
+
+	public initEmailChange(request: InitEmailChangeRequest) {
+		return this.accountService.initEmailChange(request)
+	}
+
+	public confirmEmailChange(request: ConfirmEmailChangeRequest) {
+		return this.accountService.confirmEmailChange(request)
+	}
+
+	public initPhoneChange(request: InitPhoneChangeRequest) {
+		return this.accountService.initPhoneChange(request)
+	}
+
+	public confirmPhoneChange(request: ConfirmPhoneChangeRequest) {
+		return this.accountService.confirmPhoneChange(request)
 	}
 }
