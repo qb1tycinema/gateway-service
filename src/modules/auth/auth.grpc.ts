d156134edecb850a355c7cc3dc37f5558aa8a555
qb1ty <arsenbro0707@mail.ru>
2026-08-24 06:41:40 +0500

    feat(auth): add grpc client wrapper for auth service communications

--- src/modules/auth/auth.grpc.ts
+++ src/modules/auth/auth.grpc.ts
@@ -4,6 +4,7 @@ import type {
 	AuthServiceClient,
 	RefreshRequest,
 	SendOtpRequest,
+	TelegramVerifyRequest,
 	VerifyOtpRequest
 } from "@qb1tycinema/contracts/gen/auth"
 
@@ -31,5 +32,13 @@ export class AuthClientGrpc implements OnModuleInit {
 
 	public refresh(request: RefreshRequest) {
 		return this.authService.refresh(request)
+	}
+
+	public telegramInit() {
+		return this.authService.telegramInit({})
+	}
+
+	public telegramVerify(request: TelegramVerifyRequest) {
+		return this.authService.telegramVerify(request)
 	}
 }
