e3495bdaea5b29e6a7fb94b9985bfadd828a6d42
qb1ty <arsenbro0707@mail.ru>
2026-09-14 22:39:45 +0500

    feat(gateway): implement AccountClientGrpc using AbstractGrpcClient

--- src/modules/account/account.grpc.ts
+++ src/modules/account/account.grpc.ts
@@ -1,45 +1,15 @@
-import { Inject, Injectable, OnModuleInit } from "@nestjs/common"
+import { Injectable } from "@nestjs/common"
 import type { ClientGrpc } from "@nestjs/microservices"
-import type {
-	AccountServiceClient,
-	ConfirmEmailChangeRequest,
-	ConfirmPhoneChangeRequest,
-	GetAccountRequest,
-	InitEmailChangeRequest,
-	InitPhoneChangeRequest
-} from "@qb1tycinema/contracts/gen/account"
+import { InjectGrpcClient } from "@qb1tycinema/common"
+import type { AccountServiceClient } from "@qb1tycinema/contracts/gen/account"
+
+import { AbstractGrpcClient } from "@/shared/grpc"
 
 @Injectable()
-export class AccountClientGrpc implements OnModuleInit {
-	private accountService!: AccountServiceClient
-
+export class AccountClientGrpc extends AbstractGrpcClient<AccountServiceClient> {
 	public constructor(
-		@Inject("ACCOUNT_PACKAGE")
-		private readonly client: ClientGrpc
-	) {}
-
-	public onModuleInit() {
-		this.accountService =
-			this.client.getService<AccountServiceClient>("AccountService")
-	}
-
-	public getAccount(request: GetAccountRequest) {
-		return this.accountService.getAccount(request)
-	}
-
-	public initEmailChange(request: InitEmailChangeRequest) {
-		return this.accountService.initEmailChange(request)
-	}
-
-	public confirmEmailChange(request: ConfirmEmailChangeRequest) {
-		return this.accountService.confirmEmailChange(request)
-	}
-
-	public initPhoneChange(request: InitPhoneChangeRequest) {
-		return this.accountService.initPhoneChange(request)
-	}
-
-	public confirmPhoneChange(request: ConfirmPhoneChangeRequest) {
-		return this.accountService.confirmPhoneChange(request)
+		@InjectGrpcClient("ACCOUNT_PACKAGE") client: ClientGrpc
+	) {
+		super(client, "AccountService")
 	}
 }
