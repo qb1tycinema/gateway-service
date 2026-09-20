f7760e467c387bec38b8f87807f10fda9baa422b
qb1ty <arsenbro0707@mail.ru>
2026-09-20 13:36:06 +0500

    fix: fix file format

--- src/observability/metrics/metrics.module.ts
+++ src/observability/metrics/metrics.module.ts
@@ -1,27 +1,31 @@
-import { Global, Module } from "@nestjs/common";
-import { makeCounterProvider, makeHistogramProvider, PrometheusModule } from "@willsoto/nestjs-prometheus"
+import { Global, Module } from "@nestjs/common"
+import {
+	makeCounterProvider,
+	makeHistogramProvider,
+	PrometheusModule
+} from "@willsoto/nestjs-prometheus"
 
 @Global()
 @Module({
-   imports: [
-    PrometheusModule.register({
-        path: "/metrics",
-        defaultMetrics: {
-            enabled: true
-        }
-    })
-   ],
-   providers: [
-        makeHistogramProvider({
-            name: "http_request_duration_seconds",
-            help: "HTTP request latency",
-            labelNames: ["service", "method", "route", "status"]
-        }),
-        makeCounterProvider({
-            name: "http_requests_total",
-            help: "Total HTTP requests",
-            labelNames: ["service", "method", "route", "status"]
-        })
-   ]
+	imports: [
+		PrometheusModule.register({
+			path: "/metrics",
+			defaultMetrics: {
+				enabled: true
+			}
+		})
+	],
+	providers: [
+		makeHistogramProvider({
+			name: "http_request_duration_seconds",
+			help: "HTTP request latency",
+			labelNames: ["service", "method", "route", "status"]
+		}),
+		makeCounterProvider({
+			name: "http_requests_total",
+			help: "Total HTTP requests",
+			labelNames: ["service", "method", "route", "status"]
+		})
+	]
 })
 export class MetricsModule {}
